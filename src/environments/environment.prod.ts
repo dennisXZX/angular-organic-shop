0e6db22b7d84ecd03e70712c77cd09c7eb6d4457
dennisboys <dennisboys@gmail.com>
2018-02-04 14:30:53 +1100

    project init

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDGRmidKWJvQFRyGw90eM5sUeefqvc3ItQ",
+    authDomain: "oshop-cb232.firebaseapp.com",
+    databaseURL: "https://oshop-cb232.firebaseio.com",
+    projectId: "oshop-cb232",
+    storageBucket: "",
+    messagingSenderId: "560694447744"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDGRmidKWJvQFRyGw90eM5sUeefqvc3ItQ",
+    authDomain: "oshop-cb232.firebaseapp.com",
+    databaseURL: "https://oshop-cb232.firebaseio.com",
+    projectId: "oshop-cb232",
+    storageBucket: "",
+    messagingSenderId: "560694447744"
+  }
 };

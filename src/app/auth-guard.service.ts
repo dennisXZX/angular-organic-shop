e9149acf8153934449eb7ede723d1fe6216eb1bc
Dennis Xiao <dennisboys@gmail.com>
2018-02-10 10:58:29 +1100

    redirected user to appropriate url after login

--- src/app/auth-guard.service.ts
+++ src/app/auth-guard.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, Router } from "@angular/router";
+import { CanActivate, Router, RouterStateSnapshot } from "@angular/router";
 import { AuthService } from "./auth.service";
 import 'rxjs/add/operator/map';
 
@@ -9,14 +9,15 @@ export class AuthGuard implements CanActivate {
   constructor(private auth: AuthService, private router: Router) { }
 
   // canActivate can returns Observable<boolean>
-  canActivate() {
+  canActivate(route, state: RouterStateSnapshot) {
     // we map over the user$ observable and return a boolean observable
     return this.auth.user$.map(user => {
       if (user) {
         return true;
       }
 
-      this.router.navigate(['/login']);
+      // returnUrl records the url where user tries to access /login
+      this.router.navigate(['/login'], { queryParams: { returnUrl: state.url } });
       return false;
     });
   }
--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -2,17 +2,24 @@ import { Injectable } from '@angular/core';
 import { AngularFireAuth } from "angularfire2/auth";
 import * as firebase from "firebase";
 import { Observable } from "rxjs/Observable";
+import { ActivatedRoute } from "@angular/router";
 
 @Injectable()
 export class AuthService {
   // Todo write a User class
   user$: Observable<firebase.User>;
 
-  constructor(private afAuth: AngularFireAuth) {
+  constructor(private afAuth: AngularFireAuth, private route: ActivatedRoute) {
     this.user$ = afAuth.authState;
   }
 
   login() {
+    // retrieve the parameter attached to url
+    const returnUrl = this.route.snapshot.queryParamMap.get('returnUrl') || '/';
+    // store the url into local storage
+    localStorage.setItem('returnUrl', returnUrl);
+
+    // sign up using AngularFireAuth
     this.afAuth.auth.signInWithRedirect(new firebase.auth.GoogleAuthProvider());
   }
 

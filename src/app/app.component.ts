765c35a870196189876f830202e19234c7bbbcf6
Dennis Xiao <dennisboys@gmail.com>
2018-02-10 11:11:13 +1100

    added user to database

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { AuthService } from "./auth.service";
 import { Router } from "@angular/router";
+import { UserService } from "./user.service";
 
 @Component({
   selector: 'app-root',
@@ -8,11 +9,18 @@ import { Router } from "@angular/router";
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  constructor (private auth: AuthService, private router: Router) {
+  constructor (
+    private userService: UserService,
+    private auth: AuthService,
+    private router: Router) {
     // when the user login status change, redirect it to appropriate url
     auth.user$.subscribe(user => {
       if (user) {
+        // store the user into database
+        userService.save(user);
+        // retrieve url from local storage
         const returnUrl = localStorage.getItem('returnUrl');
+        // redirect user to the appropriate url
         router.navigateByUrl(returnUrl);
       }
     });
